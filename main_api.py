0930975eb74a0385fb647f3795536efe47f524ac
miguel-data-mm <miguel.sanchez@multimedios.com>
2026-01-15 23:24:00 -0600

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -332,10 +332,10 @@ def post_purcharses():
         details_df_email = get_df_sheet("Processed_Details")
         today = date.today()
 
-        buffer = BytesIO()
+        """buffer = BytesIO()
         with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
             details_df_email.to_excel(writer, sheet_name='Datos', index=False)
-        buffer.seek(0)  # Volver al inicio del buffer
+        buffer.seek(0)  # Volver al inicio del buffer"""
 
         msg = MIMEMultipart()
         msg['From'] = SENDER_EMAIL
@@ -344,10 +344,10 @@ def post_purcharses():
         lista_destinatarios = [email.strip() for email in RECEIVER_EMAIL.split(',')]
         msg['To'] = ', '.join(lista_destinatarios)  # Para el encabezado del correo
             
-        msg.attach(MIMEText(f'Buenas tardes, se adjunta excel con ordenes procesadas a {today}', 'plain'))
-        excel_attachment = MIMEApplication(buffer.read())
+        msg.attach(MIMEText(f'Buenas tardes, se adjunta sheet con ordenes procesadas a {today}: https://docs.google.com/spreadsheets/d/1VA2jJgA_xLJLDFrsY_3mhylXrvRHhJ2019dBouBO8_U/edit?usp=sharing', 'plain'))
+        """excel_attachment = MIMEApplication(buffer.read())
         excel_attachment.add_header('Content-Disposition', 'attachment', filename=f'reporte_masteredi_{today}.xlsx')
-        msg.attach(excel_attachment)
+        msg.attach(excel_attachment)"""
 
         context_ssl = ssl.create_default_context()
         try:
@@ -506,6 +506,7 @@ def get_all_processed_purcharses():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
 
 
 
